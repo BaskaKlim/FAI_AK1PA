ab57ec2d1cbb87dbb171d29c1a10dcecd99ab5fb
BaskaKlim <baska.klimekova@gmail.com>
2021-01-28 15:20:01 +0100

    Domácí úkol 1 vyrazy (25. 10. 2020)

---
+++ AK1PA_ukol1/AK1PA_ukol1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AK1PA_ukol1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
