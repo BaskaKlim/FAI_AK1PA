f8fdc4d81a802306e05c8d0f3f2f80c6bd3add29
BaskaKlim <baska.klimekova@gmail.com>
2021-01-28 15:29:45 +0100

    init todo for ukol1 - vyrazy

---
+++ AK1AP_UKOL1/AK1AP_UKOL1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AK1AP_UKOL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i < 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            // TODO:1. prepiste cyklus for s pomoci cyklu while
+            // Zde vlozte kod s resenim
+            // TODO:2. prepiste cyklus for s pomoci cyklu do-while
+            // Zde vlozte kod s resenim
+            int t1 = 0;
+            int t2 = 60;
+            int t3 = 70;
+            if ((t1 > 50 && t2 > 50) || t3 > 50)
+            {
+                Console.WriteLine("uspel");
+            }
+            // TODO:3. Prepiste podminku pouze s pouzitim prikazu if, else a if-else a bez && (AND), || (OR) a bez pomocne promenne
+            // Zde vlozte kod s resenim
+        }
+    }
+}
