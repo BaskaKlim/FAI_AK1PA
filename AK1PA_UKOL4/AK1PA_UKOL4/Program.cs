b57747b2a01dbecaa91cdf317ff14fbb87c859b4
BaskaKlim <baska.klimekova@gmail.com>
2021-01-28 17:30:10 +0100

    init todo Ukol4 Insertion Sort

---
+++ AK1PA_UKOL4/AK1PA_UKOL4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AK1PA_UKOL4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole = new int[] { 1, 5, 7, 9, 11, 12, 15 };
+            int prvek = 3;
+
+            // mate pole s prvky jiz serazenymy vzestupne
+            // vlozte do pole promennou prvek, tak aby bylo pole opet serazeno, nejvyssi prvek po vlozeni z pole odstrante
+            // vase reseni musi byt platne pro ruzne dlouhe pole s minimalne jedním prvkem a ruzne hodnoty prvku
+
+            // priklady:
+            // pokud vlozite prvek s hodnotou  3, tak vysledne pole bude mit hodnotu  1, 3, 5, 7,  9, 11, 12
+            // pokud vlozite prvek s hodnotou 10, tak vysledne pole bude mit hodnotu  1, 5, 7, 9, 10, 11, 12
+            // pokud vlozite prvek s hodnotou  1, tak vysledne pole bude mit hodnotu  1, 1, 5, 7,  9, 11, 12
+
+            // zde napiste reseni
+        }
+    }
+}
