9b6b39abda3945bbd432b20cd03fc466446a331f
BaskaKlim <baska.klimekova@gmail.com>
2021-01-28 16:48:23 +0100

    init todo Ukol 3 bubblesort

---
+++ AK1PA_UKOL3/AK1PA_UKOL3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AK1PA_UKOL3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole = new int[] { 1, 9, 4, 7, 6, 9, 2 };
+
+            int prvek = 7;
+            // najdete a vypiste na konzoli index prvku s hodnotou 7 v poli (reseni: index 3)
+
+            // seradte hodnoty prvku v poli vzestupne pomoci algoritmu Bubble Sort a vypiste serazene hodnoty na konzoli
+        }
+    }
+}
