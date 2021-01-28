9741b16cec6609a9aabbd717efc3a07ffd4560fd
BaskaKlim <baska.klimekova@gmail.com>
2021-01-28 16:18:41 +0100

    init todo of UKOL2 - polia

---
+++ AK1PA_UKOL2/AK1PA_UKOL2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AK1PA_UKOL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole = new int[] { 1, 9, 4, 5, 6, 9, 2 };
+
+            // vypiste na konzoli hodnotu prvniho prvku
+
+            // vypiste na konzoli hodnotu posledniho prvku
+
+            // najdete a vypiste hodnotu nejvyssiho prvku v poli
+
+            // najdete a vypiste sumu (soucet vsech) prvku v poli
+
+        }
+    }
+}
